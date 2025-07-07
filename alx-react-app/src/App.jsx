9128d1ea53ae0fffee9b1ca192e0acbaefdf6205
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-07-07 11:55:04 +0200

    feat: render UserProfile component with sample user data (image, name, age, and bio)

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,15 +6,17 @@ import WelcomeMessage from "./components/WelcomeMessage";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Header />
+      {/* <Header />
       <MainContent />
-      <Footer />
+      <Footer /> */}
+      <UserProfile name="Bill" age={25} bio="Loves hiking and photography" />
     </>
   );
 }
