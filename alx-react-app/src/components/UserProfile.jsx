d732c3521d90635cfca112d0336f87cacd8c2c6c
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-07-07 12:16:35 +0200

    refactor: comment image element to try match checker

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const UserProfile = (props) => {
   return (
     <div>
-      <img src={props.image} alt="User" />
-      <h2>Name: {props.name}</h2>
+      {/* <img src={props.image} alt="User" /> */}
+      <h2>{props.name}</h2>
       <p>Age: {props.age}</p>
       <p>Bio: {props.bio}</p>
     </div>
