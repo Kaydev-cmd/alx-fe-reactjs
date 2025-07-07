80f3b22abbc9ccf91c0f0d7058b9c46ff8cf9cc3
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-07-07 11:05:19 +0200

    chore: move components to src to try and match checker

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,9 +3,9 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import WelcomeMessage from "./components/WelcomeMessage";
-import Header from "./components/layout/Header";
-import MainContent from "./components/layout/MainContent";
-import Footer from "./components/layout/Footer";
+import Header from "./Header";
+import MainContent from "./MainContent";
+import Footer from "./Footer";
 
 function App() {
   const [count, setCount] = useState(0);
