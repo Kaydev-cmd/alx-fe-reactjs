2091c99e04dfd9e49254ea1353c6a6c0b628f424
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-07-07 10:36:00 +0200

    refactor: change h1 and p text to match checker

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const WelcomeMessage = () => {
   return (
     <div>
-      <h1>Welcome the the ALX React App!!</h1>
-      <p>This is a simple JSX component.</p>
+      <h1>Hello everyone, I am learning React at ALX!</h1>
+      <p>I am learning about JSX!</p>
     </div>
   );
 };
