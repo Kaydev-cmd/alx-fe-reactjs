0b0171f2e84d32a36865b645adc7d94ea96101da
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-07-07 10:33:22 +0200

    feat: add WelcomeMessage component with basic JSX content

---
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const WelcomeMessage = () => {
+  return (
+    <div>
+      <h1>Welcome the the ALX React App!!</h1>
+      <p>This is a simple JSX component.</p>
+    </div>
+  );
+};
+
+export default WelcomeMessage;
