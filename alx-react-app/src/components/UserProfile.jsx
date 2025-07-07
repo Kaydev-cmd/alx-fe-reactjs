5da13e0fbb13512ce0b733ad599af94fee313a80
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-07-07 12:14:54 +0200

    refactor: refactor UserProfile component to use props instead of destructing to match checker

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const UserProfile = ({ image, name, age, bio }) => {
+const UserProfile = (props) => {
   return (
     <div>
-      <img src={image} alt="User" />
-      <h2>Name: {name}</h2>
-      <p>Age: {age}</p>
-      <p>Bio: {bio}</p>
+      <img src={props.image} alt="User" />
+      <h2>Name: {props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Bio: {props.bio}</p>
     </div>
   );
 };
