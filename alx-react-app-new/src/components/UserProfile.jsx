722cea46d78889f60c542c1f5bc018d64ab4aef7
Kaydev-cmd <keamogetswingwako2001@gmail.com>
2025-07-15 23:28:55 +0200

    style: add container styling with background, padding, layout, and bold text

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -2,10 +2,19 @@ import React from "react";
 
 const UserProfile = (props) => {
   return (
-    <div>
-      {/* <img src={props.image} alt="User" /> */}
+    <div
+      style={{
+        backgroundColor: "#f5f5f52b",
+        padding: "32px",
+        display: "flex",
+        flexDirection: "column",
+        color: "#fff",
+        borderRadius: "16px",
+      }}
+    >
+      <img src={props.image} alt="User" />
       <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
+      <p style={{ fontWeight: "bold" }}>Age: {props.age}</p>
       <p>Bio: {props.bio}</p>
     </div>
   );
