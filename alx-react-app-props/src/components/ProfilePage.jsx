2706d6cd4ad9947acbbd238c59fb0156571383bc
Kaydev-cmd <keamogetswingwako2001@gmail.com>
2025-07-16 09:40:54 +0200

    feat: add ProfilePage component to render UserInfo with user data

---
+++ alx-react-app-props/src/components/ProfilePage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import UserInfo from "./UserInfo";
+
+const ProfilePage = ({ userData }) => {
+  return <UserInfo userData={userData} />;
+};
+
+export default ProfilePage;
