45b76c30cc5d92a14516d5d939e50e9ee7f96c40
Kaydev-cmd <keamogetswingwako2001@gmail.com>
2025-07-16 10:17:15 +0200

    refactor: remove userData props

--- alx-react-app-props/src/components/ProfilePage.jsx
+++ alx-react-app-props/src/components/ProfilePage.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import UserInfo from "./UserInfo";
 
-const ProfilePage = ({ userData }) => {
-  return <UserInfo userData={userData} />;
+const ProfilePage = () => {
+  return <UserInfo />;
 };
 
 export default ProfilePage;
