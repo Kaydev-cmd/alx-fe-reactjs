6f1e687b86252ad327281d468c1014ddb979ccf1
Kaydev-cmd <keamogetswingwako2001@gmail.com>
2025-07-16 09:44:29 +0200

    feat: render ProfilePage component with userData prop

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,27 +1,26 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import WelcomeMessage from "./components/WelcomeMessage";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
+import ProfilePage from "./components/ProfilePage";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const userData = { name: "Jane Doe", email: "janedoe123@example.com" };
 
   return (
     <>
       {/* <Header />
       <MainContent />
       <Footer /> */}
-      <UserProfile
-        // image="src/assets/image_1.png"
+      {/* <UserProfile
+        image="src/assets/image_1.png"
         name="Alice"
         age={25}
         bio="Loves hiking and photography"
-      />
+      /> */}
+      <ProfilePage userData={userData} />
     </>
   );
 }
