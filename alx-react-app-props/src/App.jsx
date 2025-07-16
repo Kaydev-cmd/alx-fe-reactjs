1a9c8c59b7bf612d0bdb5d87abc6b33080ea570c
Kaydev-cmd <keamogetswingwako2001@gmail.com>
2025-07-16 10:19:10 +0200

    feat: wrap ProfilePage with userContext.Provider to pass user data via context

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -5,22 +5,16 @@ import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
 import ProfilePage from "./components/ProfilePage";
+import { userContext } from "./userContext";
 
 function App() {
   const userData = { name: "Jane Doe", email: "janedoe123@example.com" };
 
   return (
     <>
-      {/* <Header />
-      <MainContent />
-      <Footer /> */}
-      {/* <UserProfile
-        image="src/assets/image_1.png"
-        name="Alice"
-        age={25}
-        bio="Loves hiking and photography"
-      /> */}
-      <ProfilePage userData={userData} />
+      <userContext.Provider value={userData}>
+        <ProfilePage />
+      </userContext.Provider>
     </>
   );
 }
