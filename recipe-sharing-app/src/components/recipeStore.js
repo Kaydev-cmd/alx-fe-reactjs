c67c77f88e420f8627e9c088371dfbecbc00b5dc
Kaydev-cmd <keamogetswingwako2001@gmail.com>
2025-07-21 22:25:05 +0200

    feat: implement delete and update functionalities in recipe store

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -6,6 +6,16 @@ const useRecipeStore = create((set) => ({
     set((state) => ({
       recipes: [...state.recipes, newRecipe],
     })),
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
+  updateRecipe: (id, updatedData) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === id ? { ...recipe, ...updatedData } : recipe
+      ),
+    })),
   setRecipes: (recipes) => set({ recipes }),
 }));
 
