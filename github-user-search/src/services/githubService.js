6007545827fd14ef959c85bb21ba2f1922a22ec6
Kaydev-cmd <keamogetswingwako2001@gmail.com>
2025-08-01 17:01:25 +0200

    feat: add advanced GitHub user search with username, location, and repo count filters

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { BASE_URL } from "../constants";
 
-const fetchUserData = async (username) => {
+export const fetchUserData = async (username) => {
   try {
     const response = await axios.get(
       `https://api.github.com/users/${username}`
@@ -13,4 +13,36 @@ const fetchUserData = async (username) => {
   }
 };
 
-export default fetchUserData;
+export const searchUsers = async ({
+  username,
+  location,
+  repoCount,
+  page = 1,
+}) => {
+  try {
+    // Building the Query string
+
+    let queryParts = [];
+
+    if (username) queryParts.push(`${username} in:login`);
+    if (location) queryParts.push(`location:${location}`);
+    if (repoCount) queryParts.push(`repos:>=${repoCount}`);
+
+    const query = queryParts.join(" ");
+
+    const response = await axios.get(
+      `https://api.github.com/search/users?q=${encodeURIComponent(
+        query
+      )}&page=${page}&per_page=10`
+    );
+
+    return {
+      users: response.data.items,
+      hasMore: response.data.total_count > page * 10,
+    };
+    
+  } catch (err) {
+    console.error("Error performing advanced GitHub user search:", err);
+    throw err;
+  }
+};
