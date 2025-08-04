6c00ed4f71060e09cbaad1e9a2b45e2fcbcc713d
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-04 08:38:45 +0200

    feat: add sm responsive utilities to UserProfile component for small screen support

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const UserProfile = () => {
   return (
-    <div className="user-profile bg-gray-100 p-4 max-w-xs mx-auto my-20 rounded-lg shadow-lg md:p-8 md:max-w-ms">
+    <div className="user-profile bg-gray-100 sm:p-4 sm:max-w-xs mx-auto my-20 rounded-lg shadow-lg md:p-8 md:max-w-ms">
       <img
         src="/assets/images/john_doe.jpeg"
         alt="User"
-        className="rounded-full w-24 h-24 mx-auto md:w-36 md:h-36"
+        className="rounded-full sm:w-24 sm:h-24 mx-auto md:w-36 md:h-36"
       />
-      <h1 className="text-lg text-blue-800 my-4 md:text-xl">John Doe</h1>
-      <p className="text-gray-600 text-sm md:text-base">
+      <h1 className="sm:text-lg text-blue-800 my-4 md:text-xl">John Doe</h1>
+      <p className="text-gray-600 sm:text-sm md:text-base">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
