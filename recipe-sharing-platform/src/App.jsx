cf64d3cf19dbabb5ec2629b09c749e7414176da3
Keamogetswi Malau <keamogetswingwako2001@gmail.com>
2025-08-11 09:53:23 +0200

    feat: render HomePage component in main app entry

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
     <>
-      <h1 className="text-blue-500">Hello, React!</h1>
+      <HomePage />
     </>
   );
 }
